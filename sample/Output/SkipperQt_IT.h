146c972d83e6dc6877d13e42a985392f89f1a5d1
OrdanAlessandro <62878981+OrdanAlessandro@users.noreply.github.com>
2026-09-22 16:36:32 +0200

    added sample

---
+++ sample/Output/SkipperQt_IT.h
@@ -0,0 +1,9 @@
+// Generato automaticamente da Word2Chm. Non modificare a mano.
+// Compila con: HtmlHelp(hwnd, L"SkipperQt_IT.chm", HH_HELP_CONTEXT, IDH);
+#pragma once
+
+#ifndef _SKIPPERQT_IT_H_
+#define _SKIPPERQT_IT_H_
+
+
+#endif // _SKIPPERQT_IT_H_
